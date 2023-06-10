73a354edb7a70095f5885d27efc0f22ef9ce8ff2
WhiskyTea <silntmind@gmail.com>
2023-06-10 16:38:06 -0700

    so many changes
    so uh... we've added randomization, prevented repeat cards, created some catches for incorrect urls, cleaned up the layout.pug file a bit

--- routes/cards.js
+++ routes/cards.js
@@ -4,21 +4,22 @@ const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
 router.get('/:id', (req,res) => {
-    const { side } = req.query;
-
-    if(side.toLowerCase() !== 'question' && side.toLowerCase() !== 'answer' ){
-        res.redirect('../error');
+    const { side } = req.query; //what side are we on?
+    const { id } = req.params; //what card is this?
+    const name = req.cookies.name;
+    if(side.toLowerCase() !== 'question' && side.toLowerCase() !== 'answer'){ //what to load when there is no query string, or the query string is wrong
+        res.redirect(`/card/${id}?side=question`);
     }else{
-        const { id } = req.params;
-        const text = cards[id][side];
+
+        const text = cards[id][side]; //get the card info
         const hint = cards[id].hint;
-        const templateData = {text, id};
-        if (side.toLowerCase() === 'question'){
+        const templateData = {text, id, name};
+        if (side.toLowerCase() === 'question'){ //load the question
             templateData.hint = hint;
             templateData.displaySide = side.toUpperCase();
             templateData.otherSide = 'answer'
             res.render('card', templateData);
-        }else{
+        }else{ //load the answer
             templateData.displaySide = side.toUpperCase();
             templateData.otherSide = 'question'
             res.render('card', templateData);
@@ -27,17 +28,27 @@ router.get('/:id', (req,res) => {
 });
 
 router.get('/', (req,res) =>{
-    const cardNum = cards.length();
+    const { id } = req.query;
+    const currentCard = parseInt(id); //converts the id into an int for comparison later
+    const cardNum = cards.length; //this code block gets a random card id
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
     }
-    const url = `/${getRandomInt(cardNum)}?side=question`;
-    res.redirect(url);
-})
+    if (!id){
+        //if we're coming from the home page, load a random card
+        const url = `/card/${getRandomInt(cardNum)}?side=question`;
+        res.redirect(url);
+    }else{
+        //if the user is clicking the "get a new card" button, make sure we don't repeat
+        let newCard;
+        do{
+            newCard = getRandomInt(cardNum);
+        }while(newCard === currentCard)
+        const url = `/card/${newCard}?side=question`;
+        res.redirect(url);
+    }
 
-router.use((req,res,next) =>{
-    console.log('we made it to the card router')
-    next();
+
 })
 
 
